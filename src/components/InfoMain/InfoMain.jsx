45cecf4858fad9cba860916406b2c0d4e50427b7
Karen <karen.perez.0395@gmail.com>
2023-02-24 15:46:29 -0300

    logo mediaquery terminado

--- src/components/InfoMain/InfoMain.jsx
+++ src/components/InfoMain/InfoMain.jsx
@@ -12,13 +12,19 @@ export default function InfoMain() {
 				<img className="poster"src="./image/image 3.jpg" alt="posterBokunoHero" />
 			</div>
 			<div className='description'>
-				<div><h2 id='bnh'>My hero Academia</h2></div>
-				<div><p id="manga">manga</p></div>
-				<div><h6 id= "description">In a world in which most of the population is born with a Gift,
-				a different extraordinary ability in each one, it didn't
-				take long for both villains and heroes to appear ready to
-				to stop them.
-				</h6></div>
+				<div>
+					<h2 id='bnh'>My hero Academia</h2>
+				</div>
+				<div>
+					<p id="manga">manga</p>
+				</div>
+				<div>
+					<h6 id= "description">In a world in which most of the population is born with a Gift,
+					a different extraordinary ability in each one, it didn't
+					take long for both villains and heroes to appear ready to
+					to stop them.
+				</h6>
+				</div>
 			</div>
         </div>
           
