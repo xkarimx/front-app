4f0532410c4c5fdaee7f3d3f1670142d6b29b98d
Karen <karen.perez.0395@gmail.com>
2023-02-24 11:10:57 -0300

    maquetado terminado

--- src/layouts/NavBar/NavBar.jsx
+++ src/layouts/NavBar/NavBar.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './navbar.css'
 
 export default function NavBar() {
-    let img1 = <img src="./image/menu.png" alt="" />
-    let img2 = <img src="./image/Logo(1) 3.png" alt="logo" />
   return (
     <nav className="header-box">
-        {img1}
-        {img2}
+        <div>
+          <img src="./image/menu.png" alt="" />
+        </div>
+        <div>
+          <img src="./image/Logo(1) 3.png" alt="logo" />
+        </div>
     </nav>
   )
 }
