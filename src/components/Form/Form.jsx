5ea2db13d06057c2f4ea2d83ffd4e6eab8712e5f
Karen <karen.perez.0395@gmail.com>
2023-02-25 18:41:32 -0300

    ultimos retoques form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,54 +1,32 @@
-import React, { useRef } from "react";
 import "./form.css";
 import Wellcome from "../Wellcome/Wellcome";
 import Image from "../Image/Image";
-// import axios  from 'axios';
+
 
 export default function Form() {
-
-    let name = useRef();
-    let email =  useRef();
-    let password = useRef();    
-    let confirm = useRef();
-
-    function handleSubmit (event){
-            event.preventDefault();
-            console.log(name)
-            console.log(email)
-            console.log(password)
-            console.log(confirm)
-    }
-    
-
-
   return (
     <div className="register-fondo">
       <div className="register">
         <Wellcome />
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form">
           <fieldset>
             <legend>Name</legend>
-            <input ref={name} type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" />
             <Image src="./form-img/profile.svg" />
           </fieldset>
           <fieldset>
             <legend>Email</legend>
-            <input ref={email} type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" />
             <Image src="./form-img/@.svg" />
           </fieldset>
           <fieldset>
             <legend>Password</legend>
-            <input ref={password} type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" />
             <Image src="./form-img/lock1.svg" />
           </fieldset>
           <fieldset>
             <legend>Confirm Password</legend>
-            <input
-            ref={confirm}
-              type="password"
-              name="confirm-password"
-              id="confirm-password"
-            />
+            <input type="password" name="confirm-password" id="confirm-password"/>
             <Image src="./form-img/lock1.svg" />
           </fieldset>
           <fieldset className="notification-check">
