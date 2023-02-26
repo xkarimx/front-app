2f93b3f561991d377e5b99aa65cdbaacb9b0d6c7
Karen <karen.perez.0395@gmail.com>
2023-02-26 19:22:06 -0300

    primeros cambios

---
+++ src/components/FormRegister/FormRegister.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FormRegister() {
+  return (
+    <div className='conteiner'>
+			  <div className='conteiner-form'>
+			    <Form/>
+			  </div>	
+			  <div className='conteiner-img'>
+			    <img className="img-form" src="./image/formImg.png" alt="Img-Pagina-Form" />
+			  </div>
+		  </div>
+  )
+}
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './hero.css'
+
+export default function Hero() {
+  return (
+        <div>
+            <section className="header">
+            <NavBar />
+            <HeaderInfo />
+            </section>
+            <InfoMain />     
+        </div>
+  )
+}
