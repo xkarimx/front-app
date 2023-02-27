f5ecab295fc1725c581a013404629f81c5c48090
Karen <karen.perez.0395@gmail.com>
2023-02-27 00:26:22 -0300

    layout terminado

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../../components/Navbar/NavBar'
+
+export default function MainLayout() {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
