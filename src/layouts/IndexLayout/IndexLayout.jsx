58dea3b1aa1d11b6ebc1b5ede9570a0f6380fb87
Karen Perez <110954629+xkarimx@users.noreply.github.com>
2023-02-27 00:39:27 -0300

    M 04 layout (#12)
    * menu desplegable
    
    * menu terminado
    
    * layout terminado
    
    * ultimo retoque menu

---
+++ src/layouts/IndexLayout/IndexLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Index  from '../../components/Index/Index'
+
+export default function IndexLayout() {
+  return (
+        <div>
+            <Index/>
+        </div>
+  )
+}
